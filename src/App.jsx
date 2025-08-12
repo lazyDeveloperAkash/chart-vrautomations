7723d440b7075da8ed5ff207de4e8e3c0d4d392f
lazyDeveloperAkash@gmail.com <lazyDeveloperAkash@gmail.com>
2025-08-12 12:48:19 +0530

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import DynamicChart from './components/DynamicChart'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './components/About'
 
 const App = () => {
   return (
-    <div className='flex items-center justify-center h-[100vh] w-[100vw]'>
-      <DynamicChart/>
+    <div className='flex items-center justify-center w-[100vw] mt-10'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<DynamicChart/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
